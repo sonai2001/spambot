a5fced5e3fa62ea356a6b2088f4ed6127eefb78e
sonai2001 <82120563+sonai2001@users.noreply.github.com>
2022-07-23 20:23:37 +0530

    Add files via upload

---
+++ spam_bot.py
@@ -0,0 +1,13 @@
+import time
+import pyautogui
+
+def sendspambot():
+    time.sleep(10)
+
+    # text = open('spam.txt')
+
+    for i in range(1000):
+            pyautogui.typewrite(" erom bolle hoi <3 ")
+            pyautogui.press("enter")
+
+sendspambot()
